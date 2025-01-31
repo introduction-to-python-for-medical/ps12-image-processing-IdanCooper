d9299b922742969aee635c809dcbf07952f525cf
IdanCooper <idan738@gmail.com>
2025-02-01 00:38:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ from image_utils.py import load_image , edge_detection
 image = load_image('shoobydooby.jpg')
 clean_image = median(image, ball(3))
 binary_image = edge_detection(clean_image) <20
-plt.imsave("binary_image.png", binary_image, cmap='gray')
+
