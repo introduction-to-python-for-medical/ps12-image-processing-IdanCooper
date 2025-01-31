fc7409b3a8867905d4d3f1e0cb50d4059d9b7cff
IdanCooper <idan738@gmail.com>
2025-02-01 00:47:36 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -19,10 +19,7 @@ def edge_detection(image):
     image = np.pad(image,pad_width =((1,1),(1,1)) ,constant_values=0)
     y_filter = np.array([[1,2,1],[0,0,0],[-1,-2,-1]])
     x_filter = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
-    edgeX = convolve2d(image, x_filter)
-    edgeY = convolve2d(image, y_filter)
-    edgeX = edgeX[2:-2,2:-2]
-    edgeY = edgeY[2:-2,2:-2]
-
+    edgeX = convolve2d(image, x_filter, mode="same", boundary="fill", fillvalue=0)
+    edgeY = convolve2d(image, y_filter, mode="same", boundary="fill", fillvalue=0)
     edgeMAG = (edgeX**2 + edgeY**2)**0.5
     return edgeMAG
