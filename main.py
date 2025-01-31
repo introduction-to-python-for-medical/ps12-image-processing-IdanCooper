75a63685879a69cb840770265f4f1a670f2956cc
IdanCooper <idan738@gmail.com>
2025-02-01 01:25:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,6 @@ from image_utils.py import load_image , edge_detection
 image = load_image('shoobydooby.jpg')
 clean_image = median(image, ball(3))
 edge_detected_image = edge_detection(clean_image)
-plt.imsave("edge_detected_image", edge_detected_image, cmap='gray')
+edge_detected_image = edge_detected_image.astype(np.uint8)  
+edge_image = Image.fromarray(edge_detected_image)
+edge_image.save('my_edges.png')
